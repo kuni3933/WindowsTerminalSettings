834154c49ffb884d9a80ecc3531387d2c0355c82
kuni3933 <57650186+kuni3933@users.noreply.github.com>
2021-04-11 19:21:01 +0900

    Update 'mnt\windows\power-shell\Microsoft.PowerShell_profile.ps1'
    Signed-off-by: kuni3933 <57650186+kuni3933@users.noreply.github.com>

--- mnt/windows/power-shell/Microsoft.PowerShell_profile.ps1
+++ mnt/windows/power-shell/Microsoft.PowerShell_profile.ps1
@@ -3,11 +3,16 @@ winfetch.PS1
 Set-PSReadLineOption -PredictionSource History
 # (optional) Ctrl+f 入力で前方1単語進む : 補完の確定に使う用
 Set-PSReadLineKeyHandler -Key "Ctrl+f" -Function ForwardWord
+Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
 #参考-https://serverfault.com/questions/36991/windows-powershell-vim-keybindings
+#参考 https://docs.microsoft.com/ja-jp/powershell/module/psreadline/about/about_psreadline?view=powershell-7.1
 
 Set-PSReadLineOption -HistoryNoDuplicates:$true
 Set-PSReadLineOption -HistorySearchCursorMovesToEnd:$true
 #参考https://docs.microsoft.com/en-us/powershell/module/psreadline/set-psreadlineoption?view=powershell-7.1
+
+# Windowsベース
+Set-PSReadLineOption -EditMode Windows
 #-----------------------------------------------------
 # General
 #-----------------------------------------------------
@@ -27,7 +32,7 @@ Set-PSReadlineOption -BellStyle None
 #-----------------------------------------------------
 
 # Emacsベース
-Set-PSReadLineOption -EditMode Emacs
+#Set-PSReadLineOption -EditMode Emacs
 
 #-----------------------------------------------------
 # Powerline
