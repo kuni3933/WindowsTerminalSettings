42436f87e601b9ca41b15aa9160e0e5c80d489a2
kuni3933 <57650186+kuni3933@users.noreply.github.com>
2021-06-12 03:14:48 +0900

    Update linux/upgrade.sh
    Signed-off-by: kuni3933 <57650186+kuni3933@users.noreply.github.com>

--- linux/upgrade.sh
+++ linux/upgrade.sh
@@ -8,31 +8,55 @@ br(){
   echo ""
   echo ""
 }
-title 'sudo apt update && sudo apt upgrade'
-sudo apt update && sudo apt upgrade
+
+title 'sudo apt update -y && sudo apt upgrade -y'
+sudo apt update -y && sudo apt upgrade -y
 br
 
-title 'sudo apt-get update && sudo apt-get upgrade'
-sudo apt-get update && sudo apt-get upgrade
+title 'sudo apt full-upgrade -y'
+sudo apt full-upgrade -y
 br
 
-title 'sudo apt install git-all'
+title 'sudo apt autoremove -y && sudo apt autoclean -y'
+sudo apt autoremove -y
+sudo apt autoclean -y
+br
+
+title 'sudo apt-get update -y && sudo apt-get upgrade -y'
+sudo apt-get update -y && sudo apt-get upgrade -y
+br
+
+title 'sudo apt-get full-upgrade -y'
+sudo apt-get full-upgrade -y
+br
+
+title 'sudo apt-get autoremove -y && sudo apt-get autoclean -y'
+sudo apt-get autoremove -y
+sudo apt-get autoclean -y
+br
+
+title 'sudo apt install -y git-all'
+sudo apt show git-all
 sudo apt install -y git-all
 br
 
-title 'sudo apt instal -y gnupg2'
+title 'sudo apt install -y gnupg2'
+sudo apt show gnupg2
 sudo apt install -y gnupg2
 br
 
 title 'sudo apt install -y xclip'
+sudo apt show xclip
 sudo apt install -y xclip
 br
 
 title 'sudo apt install -y neofetch'
+sudo apt show neofetch
 sudo apt install -y neofetch
 br
 
 title 'sudo apt install -y wireless-tools'
+sudo apt show wireless-tools
 sudo apt install -y wireless-tools
 br
 
