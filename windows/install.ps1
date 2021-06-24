830f6fa6540fdc7e9e8913058f0ccddaf78e3cf1
kuni3933 <57650186+kuni3933@users.noreply.github.com>
2021-06-25 04:19:45 +0900

    Update install.ps1

--- windows/install.ps1
+++ windows/install.ps1
@@ -5,36 +5,36 @@ function Write_Section($msg) {
 }
 
 function br($times) {
-    $tmp = 1
-      while ($tmp -le $times) {
-            Write-Output " ";
-                $tmp += 1
-                  }
+  $tmp = 1
+  while ($tmp -le $times) {
+    Write-Output " ";
+    $tmp += 1
+  }
 }
 
 cd $env:USERPROFILE/WindowsTerminalSettings
 
-Write_Section "PSGallery\install.ps1"
+Write_Section "PSGallery/install.ps1"
 .\windows\PSGallery\install.ps1
 br(2)
 
-Write_Section "winget\install.ps1"
+Write_Section "winget/install.ps1"
 .\windows\winget\install.ps1
 br(2)
 
-Write_Section "scoop\install.ps1"
+Write_Section "scoop/install.ps1"
 .\windows\scoop\install.ps1
 br(2)
 
-Write_Section "volta-npm\install.ps1"
+Write_Section "volta-npm/install.ps1"
 .\windows\volta-npm\install.ps1
 br(2)
 
-Write_Section "go\install.ps1"
+Write_Section "go/install.ps1"
 .\windows\go\install.ps1
 br(2)
 
-Write_Section "cargo \install.ps1"
+Write_Section "cargo/install.ps1"
 .\windows\cargo\install.ps1
 br(2)
 
