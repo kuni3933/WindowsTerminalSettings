67be12c2c9f7f3c45d578754945b569d440e38d0
kuni3933 <57650186+kuni3933@users.noreply.github.com>
2021-07-04 02:47:41 +0900

    Update
    Signed-off-by: kuni3933 <57650186+kuni3933@users.noreply.github.com>

--- mnt/linux/ubuntu/setup.sh
+++ mnt/linux/ubuntu/setup.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 #set -eu
-readonly CURRENT_DIR="$(cd "$(dirname "${BASH_SOURCE:-0}")"; pwd)/$(basename "${BASH_SOURCE:-0}")"
+#https://ytyaru.hatenablog.com/entry/2020/02/03/111111
+readonly USERPROFILE=${HOME}
+readonly CURRENT_DIR="$(cd "$(dirname "${BASH_SOURCE:-0}")"; pwd)"
 readonly SRC_DIR="${CURRENT_DIR}/.config"
 readonly SRC_FILES=$(
 	"$(find "${SRC_DIR}" -type f)"
 )
 readonly DEST_DIR="${HOME}/.config"
+
 
 title(){
   echo ""
@@ -22,19 +25,13 @@ section(){
 }
 
 
+echo "USERPROFILE : ${USERPROFILE}"
+echo "CURRENT_DIR : ${CURRENT_DIR}"
+echo "SRC_DIR     : ${SRC_DIR}"
+echo "SRC_FILES   : ${SRC_FILES}"
+echo "DEST_DIR    : ${DEST_DIR}"
 #--------------------------------------------------------------------------------------------------"
-section '01. PPA Setup'
-#--------------------------------------------------------------------------------------------------"
-title 'sudo add-apt-repository -y ppa:regolith-linux/unstable'
-#https://github.com/regolith-linux/i3-gaps-wm
-sudo add-apt-repository -y ppa:regolith-linux/unstable
-
-title 'sudo add-apt-repository -y github/gh'
-curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo gpg --dearmor -o /usr/share/keyrings/githubcli-archive-keyring.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
-
-#--------------------------------------------------------------------------------------------------"
-section '02. Package Update'
+section '01. Package Update'
 #--------------------------------------------------------------------------------------------------"
 title 'sudo apt update -y && sudo apt upgrade -y'
 sudo apt update -y && sudo apt upgrade -y
@@ -58,7 +55,7 @@ sudo apt-get autoclean -y
 
 
 #--------------------------------------------------------------------------------------------------"
-section '03. Core Setup'
+section '02. Core Setup'
 #--------------------------------------------------------------------------------------------------"
 title 'sudo apt install -y ubuntu-wsl'
 sudo apt install -y ubuntu-wsl
@@ -78,6 +75,12 @@ sudo apt install -y make
 title 'sudo apt install -y ntp'
 sudo apt install -y ntp
 
+title 'sudo apt install -y apt-file'
+sudo apt install -y apt-file
+
+title 'sudo apt install -y software-properties-common'
+sudo apt install -y software-properties-common
+
 title 'sudo apt install -y git-all'
 sudo apt install -y git-all
 
@@ -87,6 +90,24 @@ sudo apt install -y gh
 title 'curl https://get.volta.sh | bash'
 curl https://get.volta.sh | bash
 volta setup
+
+
+#--------------------------------------------------------------------------------------------------"
+section '03. PPA Setup'
+#--------------------------------------------------------------------------------------------------"
+title 'sudo add-apt-repository -y ppa:regolith-linux/unstable'
+#https://github.com/regolith-linux/i3-gaps-wm
+sudo add-apt-repository -y ppa:regolith-linux/unstable
+
+title 'sudo add-apt-repository -y github/gh'
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo gpg --dearmor -o /usr/share/keyrings/githubcli-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+
+title 'sudo apt update -y && sudo apt upgrade -y'
+sudo apt update -y && sudo apt upgrade -y
+
+title 'sudo apt-get update -y && sudo apt-get upgrade -y'
+sudo apt-get update -y && sudo apt-get upgrade -y
 
 
 #--------------------------------------------------------------------------------------------------"
@@ -108,13 +129,13 @@ title 'sudo apt install fonts-symbola'
 sudo apt install -y fonts-symbola
 
 title 'git clone --depth 1 https://github.com/ryanoasis/nerd-fonts.git'
-if [ -e {/usr/share/fonts/truetype/source-code-pro-ttf} ]; then
-  exho "Already installed."
+if [ -d /usr/share/fonts/truetype/source-code-pro-ttf ]; then
+  echo "Already installed."
 else
-  cd ~/
-  git clone --depth 1 https://github.com/ryanoasis/nerd-fonts.git
-  ~/nerd-fonts/install.sh SourceCodePro
-  rm -rf nerd-fonts/
+  git clone --depth 1 https://github.com/ryanoasis/nerd-fonts.git /tmp/nerd-fonts
+  /tmp/nerd-fonts/install.sh SourceCodePro
+  sudo rm -rf /tmp/nerd-fonts/
+  cd ${CURRENT_DIR}
 fi
 
 title 'sudo update-locale LANG=ja_JP.UTF-8'
@@ -125,30 +146,35 @@ sudo update-locale LANG=ja_JP.UTF-8
 section '05. Terminal Setup'
 #--------------------------------------------------------------------------------------------------"
 title 'git clone https://github.com/Bash-it/bash-it.git'
-if [ -e {~/.bash_it} ]; then
+if [ -d ${USERPROFILE}/.bash_it ]; then
     # 存在する場合
     echo 'Already Installed.'
 else
     # 存在しない場合
-    git clone https://github.com/Bash-it/bash-it.git ~/bash-it
-    ~/.bash_it/install.sh
+    git clone https://github.com/Bash-it/bash-it.git ${USERPROFILE}/.bash-it
+    ${USERPROFILE}/.bash_it/install.sh
 fi
 
 title 'ln -sf ${CURRENT_DIR}/.bash_it/themes/maman'
-if [ -e {~/.bash_it/themes/maman} ]; then
+if [ -d ${USERPROFILE}/.bash_it/themes/maman ]; then
     # 存在する場合
     echo "Already Installed."
 else
     # 存在しない場合
-    ln -sf "${CURRENT_DIR}/.bash_it/themes/maman/maman.theme.bash" "~/.bash_it/themes/maman/maman.theme.bash"
+    sudo ln -sf ${CURRENT_DIR}/.bash_it/themes/maman/maman.theme.bash ${USERPROFILE}/.bash_it/themes/maman/maman.theme.bash
 fi
 
-title 'ln -sf "${CURRENT_DIR}/.bashrc ~/.bashrc.org'
-ln -sf "${CURRENT_DIR}/.bashrc" "~/.bashrc.org"
-echo '. ~/.bashrc.org' >> ~/.bashrc
+title 'ln -sf ${CURRENT_DIR}/.bashrc ~/.bashrc.org'
+if [ -d ${USERPROFILE}/.bashrc.org ]; then
+  #既にある場合
+  echo "Already Installed."
+else
+  sudo ln -sf ${CURRENT_DIR}/.bashrc ${USERPROFILE}/.bashrc.org
+  echo '. ~/.bashrc.org' >> ${USERPROFILE}/.bashrc
+fi
 
 title 'ln -sf ${CURRENT_DIR}/.inputrc ~/.inputrc'
-ln -sf "${CURRENT_DIR}/.inputrc" "~/.inputrc"
+sudo ln -sf ${CURRENT_DIR}/.inputrc ${USERPROFILE}/.inputrc
 
 title 'sudo apt install -y fzf'
 sudo apt install -y fzf
@@ -157,14 +183,14 @@ title 'sudo apt install -y tmux'
 sudo apt install -y tmux
 
 title 'git clone --depth 1 https://github.com/jimeh/tmux-themepack.git ~/.tmux-themepack'
-if [ -e {~/.tmux-themepack} ]; then↲
+if [ -d ${USERPROFILE}/.tmux-themepack ]; then
   echo "Already Installed."
 else
-  git clone --depth 1 https://github.com/jimeh/tmux-themepack.git ~/.tmux-themepack
+  git clone --depth 1 https://github.com/jimeh/tmux-themepack.git ${USERPROFILE}/.tmux-themepack
 fi
 
 title 'ln -sf ${CURRENT_DIR}/.tmux.conf ~/.tmux.conf'
-ln -sf "${CURRENT_DIR}/.tmux.conf" "~/.tmux.conf"
+sudo ln -sf ${CURRENT_DIR}/.tmux.conf ${USERPROFILE}/.tmux.conf
 
 
 #-------------------------------------------------------------------------------------------------------"
@@ -188,8 +214,8 @@ sudo apt install -y libc6-dev
 #--------------------------------------------------------------------------------------------------"
 section '# Go'
 #--------------------------------------------------------------------------------------------------"
-title 'sudo ~/WindowsTerminalSettings/mnt/linux/ubuntu/update-golang/update-golang.sh'
-sudo ${CURRENT_DIR}/update-golang/update-golang.sh
+title 'sudo ./mnt/linux/ubuntu/update-golang/update-golang.sh'
+# sudo ${CURRENT_DIR}/update-golang/update-golang.sh
 
 #--------------------------------------------------------------------------------------------------"
 section '# Python Install'
@@ -212,16 +238,17 @@ title 'sudo apt install -y python3-venv'
 sudo apt install -y python3-venv
 
 title 'git clone --depth 1 https://github.com/pyenv/pyenv.git ~/.pyenv'
-if [ -e {~/.pyenv} ]; then
+if [ -d ${USERPROFILE}/.pyenv ]; then
     # 存在する場合
-    rm -rf ~/.pyenv
+    sudo rm -rf ${USERPROFILE}/.pyenv
 fi
-if [ -e {/usr/bin/python3} ]; then
+if [ -f /usr/bin/python3 ]; then
   # 存在する場合
      sudo apt remove -y python3
 fi
-git clone --depth 1 https://github.com/pyenv/pyenv.git ~/.pyenv
-ln -sf "/usr/bin/python3" "/usr/bin/python"
+title 'git clone --depth 1 https://github.com/pyenv/pyenv.git ~/.pyenv'
+git clone --depth 1 https://github.com/pyenv/pyenv.git ${USERPROFILE}/.pyenv
+sudo ln -sf /usr/bin/python3 /usr/bin/python
 
 title 'pyenv install 3.9.5'
 pyenv install 3.9.5
@@ -229,15 +256,28 @@ pyenv install 3.9.5
 title 'curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/install-poetry.py | python -'
 curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/install-poetry.py | python -
 
+title 'pip install -U pip'
+pip install -U pip
+
 #--------------------------------------------------------------------------------------------------"
 section '# rust'
 #--------------------------------------------------------------------------------------------------"
 title 'curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh'
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+title 'rustup install nightly-x86_64-unknown-linux-gnu'
 rustup install nightly-x86_64-unknown-linux-gnu
+
+title 'rustup default nightly-x86_64-unknown-linux-gnu'
 rustup default nightly-x86_64-unknown-linux-gnu
+
+title 'rustup self update'
 rustup self update
+
+title 'rustup update'
 rustup update
+
+title 'rustup component add'
 rustup component add clippy rls rust-analysis rust-src rust-docs rustfmt
 
 #--------------------------------------------------------------------------------------------------"
@@ -276,26 +316,32 @@ title 'sudo apt install -y vim'
 sudo apt install -y vim
 
 title 'git clone --depth 1 https://github.com/VundleVim/Vundle.vim.git'
-if [ -e {~/.vim} ]; then
+if [ -d ${USERPROFILE}/.vim ]; then
     # 存在する場合
-    cd ~/.vim
-    git clone --depth 1 https://github.com/VundleVim/Vundle.vim.git
+    if [ -d ${USERPROFILE}/.vim/Vundle.vim ]; then
+      echo "Already Installed."↲
+    else
+      git clone --depth 1 https://github.com/VundleVim/Vundle.vim.git
+    fi
 else
     # 存在しない場合
-    cd ~/
+    cd ${USERPROFILE}/
     mkdir .vim
     git clone --depth 1 https://github.com/VundleVim/Vundle.vim.git
 fi
 
 title 'ln -sf ${CURRENT_DIR}/.vimrc ~/.vimrc'
-ln -sf "${CURRENT_DIR}/.vimrc" "~/.vimrc"
+sudo ln -sf ${CURRENT_DIR}/.vimrc ${USERPROFILE}/.vimrc
 
-title 'ln -sf ${CURRENT_DIR}/.vim-snippets ~/.vim-snippets'
-ln -sf "${CURRENT_DIR}/.vim-snippets" "~/.vim-snippets"
+title 'ln -sf /mnt/common/.vim-snippets ~/.vim-snippets'
+cd ${CURRENT_DIR}
+cd ../../common
+sudo ln -sf ./.vim-snippets ${USERPROFILE}/.vim-snippets
+cd ${CURRENT_DIR}
 
-title 'pip3 install neovim'
-pip3 install neovim
-
+title 'pip install neovim'
+pip install neovim
+pip install -U neovim
 
 #--------------------------------------------------------------------------------------------------"
 section '08. i3 & i3-gaps'
@@ -379,7 +425,7 @@ title 'sudo apt install -y fcitx-tools'
 sudo apt install -y fcitx-tools
 
 title  'sudo apt install fcitx-module-dbus'
-sudo apt install fcitx-module-dbus
+sudo apt install -y fcitx-module-dbus
 
 title 'sudo apt install -y yaru-theme-icon'
 sudo apt install -y yaru-theme-icon
@@ -392,14 +438,17 @@ sudo apt install -y yaru-theme-icon
 #--------------------------------------------------------------------------------------------------"
 section '10. CLI tool Install'
 #--------------------------------------------------------------------------------------------------"
-title 'pip3 install ansible'
-pip3 install ansible
+title 'pip install pywinrm'
+pip install pywinrm
+pip install -U pywinrm
 
-title 'pip3 install pywinrm'
-pip3 install pywinrm
+title 'pip install awscli'
+pip install awscli
+pip install -U awscli
 
-title 'pip3 install awscli'
-pip3 install awscli
+title 'pip install ansible'
+pip install ansible
+pip install -U ansible
 
 title 'cargo install --locked bat'
 cargo install --locked bat
@@ -412,13 +461,13 @@ sudo rm -rf /tmp/bats
 title 'cargo install broot'
 cargo install broot
 broot --help
-ln -sf "${CURRENT_DIR}/.config/broot/conf.toml" "~/.config/broot/conf.toml"
+sudo ln -sf ${CURRENT_DIR}/.config/broot/conf.toml ${USERPROFILE}/.config/broot/conf.toml
 
 title 'git clone --depth 1 https://github.com/universal-ctags/ctags.git /tmp/ctags'
 git clone https://github.com/universal-ctags/ctags.git /tmp/ctags
 cd /tmp/ctags
-sudo ./autogen.sh && ./configure --prefix=/usr/local && make && make install
-cd
+sudo ./autogen.sh && ./configure --prefix=/usr/local && sudo make && sudo make install
+cd ${CURRENT_DIR}
 sudo rm -rf /tmp/ctags
 
 title 'cargo install git-delta'
@@ -431,13 +480,15 @@ git config --global delta.side-by-side true
 git config --global interactive.diffFilter delta --color-only
 
 title 'git clone https://github.com/skanehira/docui.git /tmp/docui'
+git clone https://github.com/skanehira/docui.git /tmp/docui
 cd /tmp/docui
 GO111MODULE=on go install
-cd
+cd ${CURRENT_DIR}
 sudo rm -rf /tmp/docui
 
-title 'pip3 isntall docker/compose'
-pip3 install docker-compose
+title 'pip isntall docker/compose'
+pip install docker-compose
+pip install -U docker-compose
 
 title 'cargo install exa'
 cargo install exa
@@ -448,20 +499,20 @@ cargo install fd-find
 title 'npm install -g fx'
 npm install -g fx
 
-title 'pip3 install glances glances[docker,ip,web]'
-pip3 install glances glances[docker,ip,web]
+title 'pip install glances glances[docker,ip,web]'
+pip install glances glances[docker,ip,web]
 
 title 'go get -u github.com/tadashi-aikawa/gowl'
 go get -u github.com/tadashi-aikawa/gowl
 
 title 'sudo apt install jq'
-sudo apt install jq
+sudo apt install -y jq
 
 title 'sudo apt install ncdu'
-sudo apt install ncdu
+sudo apt install -y ncdu
 
 title 'sudo apt install nkf'
-sudo apt install nkf
+sudo apt install -y nkf
 
 title 'cargo install ripgrep'
 cargo install ripgrep
@@ -469,32 +520,28 @@ cargo install ripgrep
 title 'GO111MODULE=on go get mvdan.cc/sh/v3/cmd/shfmt'
 GO111MODULE=on go get mvdan.cc/sh/v3/cmd/shfmt
 
-title 'git clone https://github.com/jonas/tig.git /tmp/tig↲'
+title 'git clone https://github.com/jonas/tig.git /tmp/tig'
 git clone https://github.com/jonas/tig.git /tmp/tig
 cd /tmp/tig
-make
-make install
-cd
+sudo make
+sudo make install
+cd ${CURRENT_DIR}
 sudo rm -rf /tmp/tig
-ln -sf "${CURRENT_DIR}/.tigrc" "~/.tigrc"
+sudo ln -sf ${CURRENT_DIR}/.tigrc ${USERPROFILE}/.tigrc
 
 title 'git clone https://github.com/facebook/zstd.git ~/zstd'
-git clone https://github.com/facebook/zstd.git ~/zstd
-cd ~/zstd
-make install
-cd ~/zstd/contrib/pzstd
-make install
+git clone https://github.com/facebook/zstd.git ${USERPROFILE}/zstd
+cd ${USERPROFILE}/zstd
+sudo make install
+cd ${USERPROFILE}/zstd/contrib/pzstd
+sudo make install
 
 title 'git clone git@github.com:rupa/z.git ~/z'
-git clone git@github.com:rupa/z.git ~/z
-sudo ~/z/z.sh
-sudo rm -rf ~/z
-
-title 'sudo apt install -y apt-file'
-sudo apt install -y apt-file
-
-title 'sudo apt install -y software-properties-common'
-sudo apt install -y software-properties-common
+git clone git@github.com:rupa/z.git ${USERPROFILE}/z
+cd ${USERPROFILE}/z
+sudo make
+cd ${CURRENT_DIR}
+sudo rm -rf ${USERPROFILE}/z
 
 title 'sudo apt install -y gnupg2'
 sudo apt install -y gnupg2
