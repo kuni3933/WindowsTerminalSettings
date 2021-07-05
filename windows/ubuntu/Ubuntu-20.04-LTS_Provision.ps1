bd5f706c8f08a9438bce2dd57af08a0589bd9db6
kuni3933 <57650186+kuni3933@users.noreply.github.com>
2021-07-05 14:01:20 +0900

    Update
    Signed-off-by: kuni3933 <57650186+kuni3933@users.noreply.github.com>

--- windows/ubuntu/Ubuntu-20.04-LTS_Provision.ps1
+++ windows/ubuntu/Ubuntu-20.04-LTS_Provision.ps1
@@ -1,28 +1,30 @@
 #Ubuntu-20.04-LTS
-# 冪等性はないので注意
+# 等性はないので注意
 # 実行後に編集が必要なファイルもある
-cp $home\.gitconfig \\wsl$\Ubuntu-20.04\tmp\
-wsl -- mv /tmp/.gitconfig ~/
+if(Test-Path $home/.gitconfig){
+  cp $home\.gitconfig \\wsl$\Ubuntu-20.04\tmp\
+  wsl -- mv /tmp/.gitconfig ~/
+}
 # root/browserなどpathの変更が必要
-cp $home\.gowlconfig \\wsl$\Ubuntu-20.04\tmp
-wsl -- mv /tmp/.gowlconfig ~/
-
+if(Test-Path $home/.gitconfig){
+  cp $home\.gowlconfig \\wsl$\Ubuntu-20.04\tmp
+  wsl -- mv /tmp/.gowlconfig ~/
+}
 # pathの変更が必要かも..
-cp -r $home\.ssh \\wsl$\Ubuntu-20.04\tmp\
-wsl -- rm -rf ~/.ssh
-wsl -- mv /tmp/.ssh ~/
-wsl -- chmod 600 ~/.ssh/*
-
+if(Test-Path $home/.ssh){
+  cp -r $home\.ssh \\wsl$\Ubuntu-20.04\tmp\
+  wsl -- rm -rf ~/.ssh
+  wsl -- mv /tmp/.ssh ~/
+  wsl -- chmod 600 ~/.ssh/*
+}
 # pathの変更が必要かも..
-cp -r $home\.gnupg \\wsl$\Ubuntu-20.04\tmp\
-wsl -- rm -rf ~/.gnupg
-wsl -- mv /tmp/.gnupg ~/
-wsl -- chmod 600 ~/.gnupg/*
-
-cp $home\WindowsTerminalSettings\windows\ubuntu\wsl.conf \\wsl$\Ubuntu\tmp\
+if(Test-Path $home/.gnupg){
+  cp -r $home\.gnupg \\wsl$\Ubuntu-20.04\tmp\
+  wsl -- rm -rf ~/.gnupg
+  wsl -- mv /tmp/.gnupg ~/
+  wsl -- chmod 600 ~/.gnupg/*
+}
+cp $home\WindowsTerminalSettings\windows\ubuntu\wsl.conf \\wsl$\Ubuntu-20.04\tmp\
 wsl -- sudo mv /tmp/wsl.conf  /etc
 wsl -- sudo chmod 600 /etc/wsl.conf
-
-cp $home\WindowsTerminalSettings\windows\ubuntu\.bashrc \\wsl$\Ubuntu\tmp\
-wsl -- sudo mv --force /tmp/.bashrc  ~/
 
--- windows/ubuntu/Ubuntu_provision.ps1
+++ windows/ubuntu/Ubuntu_provision.ps1
@@ -1,28 +1,31 @@
 # Ubuntu
 # 等性はないので注意
 # 実行後に編集が必要なファイルもある
-cp $home\.gitconfig \\wsl$\Ubuntu\tmp\
-wsl -- mv /tmp/.gitconfig ~/
+if(Test-Path $home/.gitconfig){
+  cp $home\.gitconfig \\wsl$\Ubuntu\tmp\
+  wsl -- mv /tmp/.gitconfig ~/
+}
 # root/browserなどpathの変更が必要
-cp $home\.gowlconfig \\wsl$\Ubuntu\tmp
-wsl -- mv /tmp/.gowlconfig ~/
-
+if(Test-Path $home/.gitconfig){
+  cp $home\.gowlconfig \\wsl$\Ubuntu\tmp
+  wsl -- mv /tmp/.gowlconfig ~/
+}
 # pathの変更が必要かも..
-cp -r $home\.ssh \\wsl$\Ubuntu\tmp\
-wsl -- rm -rf ~/.ssh
-wsl -- mv /tmp/.ssh ~/
-wsl -- chmod 600 ~/.ssh/*
-
+if(Test-Path $home/.ssh){
+  cp -r $home\.ssh \\wsl$\Ubuntu\tmp\
+  wsl -- rm -rf ~/.ssh
+  wsl -- mv /tmp/.ssh ~/
+  wsl -- chmod 600 ~/.ssh/*
+}
 # pathの変更が必要かも..
-cp -r $home\.gnupg \\wsl$\Ubuntu\tmp\
-wsl -- rm -rf ~/.gnupg
-wsl -- mv /tmp/.gnupg ~/
-wsl -- chmod 600 ~/.gnupg/*
+ if(Test-Path $home/.gnupg){
+  cp -r $home\.gnupg \\wsl$\Ubuntu\tmp\
+  wsl -- rm -rf ~/.gnupg
+  wsl -- mv /tmp/.gnupg ~/
+  wsl -- chmod 600 ~/.gnupg/*
+}
 
 cp $home\WindowsTerminalSettings\windows\ubuntu\wsl.conf \\wsl$\Ubuntu\tmp\
 wsl -- sudo mv /tmp/wsl.conf  /etc
 wsl -- sudo chmod 600 /etc/wsl.conf
-
-cp $home\WindowsTerminalSettings\windows\ubuntu\.bashrc \\wsl$\Ubuntu\tmp\
-wsl -- sudo mv --force /tmp/.bashrc  ~/
 
