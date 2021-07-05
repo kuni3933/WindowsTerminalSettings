ea5d1737a30005938d38b2c40772cb522ff6a48d
kuni3933 <57650186+kuni3933@users.noreply.github.com>
2021-07-05 14:49:10 +0900

    Update Ubuntu_Provision
    Signed-off-by: kuni3933 <57650186+kuni3933@users.noreply.github.com>

--- windows/ubuntu/Ubuntu_provision.ps1
+++ windows/ubuntu/Ubuntu_provision.ps1
@@ -1,31 +1,50 @@
+function Write_Title($msg) {
+  Write-Host ""
+  Write-Host "┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━" -ForegroundColor Yellow
+  Write-Host "┃$msg" -ForegroundColor Yellow
+  Write-Host "┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━" -ForegroundColor Yellow
+}
+
 # Ubuntu
 # 等性はないので注意
 # 実行後に編集が必要なファイルもある
-if(Test-Path $home/.gitconfig){
+Write_Title 'cp $USERPROFILE\.gitconfig \\wsl$\Ubuntu\$home\'
+if (Test-Path $home/.gitconfig) {
   cp $home\.gitconfig \\wsl$\Ubuntu\tmp\
   wsl -- mv /tmp/.gitconfig ~/
 }
+
 # root/browserなどpathの変更が必要
-if(Test-Path $home/.gitconfig){
+Write_Title 'cp $USERPROFILE\.gowlconfig \\wsl$\Ubuntu\$home\'
+if (Test-Path $home/.gowlconfig) {
   cp $home\.gowlconfig \\wsl$\Ubuntu\tmp
   wsl -- mv /tmp/.gowlconfig ~/
 }
+
 # pathの変更が必要かも..
-if(Test-Path $home/.ssh){
+Write_Title 'cp $USERPROFILE\.ssh \\wsl$\Ubuntu\$home\'
+if (Test-Path $home/.ssh) {
   cp -r $home\.ssh \\wsl$\Ubuntu\tmp\
   wsl -- rm -rf ~/.ssh
   wsl -- mv /tmp/.ssh ~/
   wsl -- chmod 600 ~/.ssh/*
 }
+
 # pathの変更が必要かも..
- if(Test-Path $home/.gnupg){
+Write_Title 'cp -r $USERPROFILE\.gnupg \\wsl$\Ubuntu\$home\'
+if (Test-Path $home/.gnupg) {
   cp -r $home\.gnupg \\wsl$\Ubuntu\tmp\
   wsl -- rm -rf ~/.gnupg
   wsl -- mv /tmp/.gnupg ~/
   wsl -- chmod 600 ~/.gnupg/*
 }
 
+Write_Title 'cp $USERPROFILE\WindowsTerminalSettings\windows\ubuntu\wsl.conf \\wsl$\Ubuntu\etc\'
 cp $home\WindowsTerminalSettings\windows\ubuntu\wsl.conf \\wsl$\Ubuntu\tmp\
 wsl -- sudo mv /tmp/wsl.conf  /etc
 wsl -- sudo chmod 600 /etc/wsl.conf
 
+Write_Title 'sudo apt install -y git-all'
+wsl -- sudo apt update -y
+wsl -- sudo apt upgrade -y
+wsl -- sudo apt install -y git-all
