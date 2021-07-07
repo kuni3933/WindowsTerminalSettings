9ec4d8788118d1e43b24acb64d0d9e8b693b2d40
kuni3933 <57650186+kuni3933@users.noreply.github.com>
2021-07-07 17:14:48 +0900

    Update
    Signed-off-by: kuni3933 <57650186+kuni3933@users.noreply.github.com>

--- windows/ubuntu/Ubuntu_provision.ps1
+++ windows/ubuntu/Ubuntu_provision.ps1
@@ -4,39 +4,74 @@ function Write_Title($msg) {
   Write-Host "┃$msg" -ForegroundColor Yellow
   Write-Host "┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━" -ForegroundColor Yellow
 }
+${USERNAME}=wsl -- whoami
+Write_Title "Ubuntu_USERNAME : ${USERNAME}"
+wsl --terminate Ubuntu
 
 # Ubuntu
 # 等性はないので注意
 # 実行後に編集が必要なファイルもある
-Write_Title 'cp $USERPROFILE\.gitconfig \\wsl$\Ubuntu\$home\'
+Write_Title "cp $USERPROFILE\.gitconfig \\wsl$\Ubuntu\home\${USERNAME}\"
 if (Test-Path $home/.gitconfig) {
+  if(Test-Path "\\wsl$\Ubuntu\home\${USERNAME}\.gitconfig"){
+    Write-Host "Already installed." 
+  }
+  else{
   cp $home\.gitconfig \\wsl$\Ubuntu\tmp\
   wsl -- mv /tmp/.gitconfig ~/
+  }
+}
+else{
+  Write-Host "$home/.gitconfig IS NOT FOUND."
 }
 
 # root/browserなどpathの変更が必要
-Write_Title 'cp $USERPROFILE\.gowlconfig \\wsl$\Ubuntu\$home\'
+Write_Title "cp $USERPROFILE\.gowlconfig \\wsl$\Ubuntu\home\${USERNAME}\"
 if (Test-Path $home/.gowlconfig) {
+  if(Test-Path "\\wsl$\Ubuntu\home\${USERNAME}\.gowlconfig"){
+    Write-Host "Already installed."
+  }
+  else{
   cp $home\.gowlconfig \\wsl$\Ubuntu\tmp
   wsl -- mv /tmp/.gowlconfig ~/
+  }
+}
+else{
+  Write-Host "$home/.gowlconfig IS NOT FOUND."
 }
 
 # pathの変更が必要かも..
-Write_Title 'cp $USERPROFILE\.ssh \\wsl$\Ubuntu\$home\'
+Write_Title "cp $USERPROFILE\.ssh \\wsl$\Ubuntu\home\${USERNAME}\"
 if (Test-Path $home/.ssh) {
+  if(Test-Path "\\wsl$\Ubuntu\home\${USERNAME}\.ssh"){
+    Write-Host "Already installed."
+  }
+  else{
   cp -r $home\.ssh \\wsl$\Ubuntu\tmp\
   wsl -- rm -rf ~/.ssh
   wsl -- mv /tmp/.ssh ~/
   wsl -- chmod 600 ~/.ssh/*
+  }
+}
+else{
+  Write-Host "$home/.ssh IS NOT FOUND."
 }
 
 # pathの変更が必要かも..
-Write_Title 'cp -r $USERPROFILE\.gnupg \\wsl$\Ubuntu\$home\'
+Write_Title "cp -r $USERPROFILE\.gnupg \\wsl$\Ubuntu\home\${USERNAME}\"
 if (Test-Path $home/.gnupg) {
+  if(Test-Path "\\wsl$\Ubuntu\home\${USERNAME}\.gnupg"){
+    Write-Host "Already installed."
+  }
+  else{
   cp -r $home\.gnupg \\wsl$\Ubuntu\tmp\
   wsl -- rm -rf ~/.gnupg
   wsl -- mv /tmp/.gnupg ~/
   wsl -- chmod 600 ~/.gnupg/*
+  }
+}
+else{
+  Write-Host "$home/.gnupg IS NOT FOUND."
 }
 
 Write_Title 'cp $USERPROFILE\WindowsTerminalSettings\windows\ubuntu\wsl.conf \\wsl$\Ubuntu\etc\'
