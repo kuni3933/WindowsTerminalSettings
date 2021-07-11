cc3d5c784d61bcbbc8122a569ac0200c7cf6fb22
kuni3933 <57650186+kuni3933@users.noreply.github.com>
2021-07-12 02:38:40 +0900

    Update linux/Pull_SubModule.sh
    Signed-off-by: kuni3933 <57650186+kuni3933@users.noreply.github.com>

--- linux/Pull_SubModule.sh
+++ linux/Pull_SubModule.sh
@@ -16,32 +16,31 @@ br(){
 }
 
 # 参考 - https://devconnected.com/how-to-check-if-file-or-directory-exists-in-bash/
-if [[! -d "~/WindowsTerminalSettings/" ]]
-then
-  section "Error"
-  echo "Repository:WindowsTerminalSettings が $home に存在しないないためサブモジュールのpullが出来ません."
-  echo "Repository:WindowsTerminalSettings を $home に git cloneして配置してください."
-  echo "Cannot pull submodules because Repository:WindowsTerminalSettings does not exist in $home."
-  echo "Git clone Repository:WindowsTerminalSettings to $home and place it there."
-  echo "参考-Reference:https://github.com/kuni3933/WindowsTerminalSettings"
+if [ -e ~/WindowsTerminalSettings ]; then
+    cd ~/WindowsTerminalSettings/
+    title "現在のサブモジュールステータス / Current submodule status"
+    git submodule status
+    br
+
+    title "git submodule foreach git fetch"
+    git submodule foreach git fetch
+    br
+
+    title "git submodule foreach git pull origin master"
+    git submodule foreach git pull origin master
+    br
+
+    title "Pull後のサブモジュールステータス / Submodule status after Pull"
+    git submodule status
+    br
+
+    section "Pull_SubModule.sh is complete."
 else
-  cd ~/WindowsTerminalSettings/
-  title "現在のサブモジュールステータス / Current submodule status"
-  git submodule status
-  br
-
-  title "git submodule foreach git fetch"
-  git submodule foreach git fetch
-  br
-
-  title "git submodule foreach git pull origin master"
-  git submodule foreach git pull origin master
-  br
-
-  title "Pull後のサブモジュールステータス / Submodule status after Pull"
-  git submodule status
-  br
-
-  section "Pull_SubModule.sh is complete."
+    section "Error"
+    echo "Repository:WindowsTerminalSettings が $HOME に存在しないないためサブモジュールのpullが出来ません."
+    echo "Repository:WindowsTerminalSettings を $HOME に git cloneして配置してください."
+    echo "Cannot pull submodules because Repository:WindowsTerminalSettings does not exist in $HOME."
+    echo "Git clone Repository:WindowsTerminalSettings to $HOME and place it there."
+    echo "参考-Reference:https://github.com/kuni3933/WindowsTerminalSettings"
 fi
 
