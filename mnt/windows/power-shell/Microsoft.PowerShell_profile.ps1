43e0412189b28e56de071f8a98967d44d91e2b9c
kuni3933 <57650186+kuni3933@users.noreply.github.com>
2021-10-05 09:36:01 +0900

    Update mnt/windows/power-shell/Microsoft.PowerShell_profile.ps1
    Signed-off-by: kuni3933 <57650186+kuni3933@users.noreply.github.com>

--- mnt/windows/power-shell/Microsoft.PowerShell_profile.ps1
+++ mnt/windows/power-shell/Microsoft.PowerShell_profile.ps1
@@ -113,10 +113,11 @@ function _fzf_compgen_dir() {
 #-----------------------------------------------------
 
 # https://secon.dev/entry/2020/08/17/070735/
+# Delete cat, mv,
 @"
   arch, base32, base64, basename, cksum, comm, cp, cut, date, df, dircolors, dirname,
   echo, env, expand, expr, factor, false, fmt, fold, hashsum, head, hostname, join, link, ln,
-  ls, md5sum, mkdir, mktemp, more, mv, nl, nproc, od, paste, printenv, printf, ptx, pwd,
+  ls, md5sum, mkdir, mktemp, more, nl, nproc, od, paste, printenv, printf, ptx, pwd,
   readlink, realpath, relpath, rm, rmdir, seq, sha1sum, sha224sum, sha256sum, sha3-224sum,
   sha3-256sum, sha3-384sum, sha3-512sum, sha384sum, sha3sum, sha512sum, shake128sum,
   shake256sum, shred, shuf, sleep, sort, split, sum, sync, tac, tail, tee, test, touch, tr,
