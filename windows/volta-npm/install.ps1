f3669cafc5798216726de4797dc9a532149b9490
kuni3933 <57650186+kuni3933@users.noreply.github.com>
2021-10-14 14:55:52 +0900

    Update
    Signed-off-by: kuni3933 <57650186+kuni3933@users.noreply.github.com>

--- windows/volta-npm/install.ps1
+++ windows/volta-npm/install.ps1
@@ -35,6 +35,10 @@ Write_Title("# npm install -g express")
 npm install -g express
 br(1)
 
+Write_Title("# npm install -g ejs")
+npm install -g ejs
+br(1)
+
 Write_Title("# volta list all")
 volta list all
 br(1)
