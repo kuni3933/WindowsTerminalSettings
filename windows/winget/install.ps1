cf8bbe3a0d61b8bc37e9ef8c7d9b1580d688ca99
kuni3933 <57650186+kuni3933@users.noreply.github.com>
2022-01-04 19:08:31 +0900

    [Update] windows/winget/install.ps1

--- windows/winget/install.ps1
+++ windows/winget/install.ps1
@@ -23,11 +23,11 @@ function Title($ID) {
     Return $ID
 }
 
-function Install_msg($ID) {
+function Install_Msg($ID) {
     Write-Host "  インストールを開始します." -ForegroundColor Yellow
     Write-Host "  Start the installation of this application." -ForegroundColor Yellow
     br(1)
-    winget show --id "$ID" --source winget
+    winget show --id "$ID"
     br(1)
 }
 
@@ -56,7 +56,7 @@ if (Test-Path "${env:PROGRAMFILES}/Git/bin/git.exe") {
     git update-git-for-windows
 }
 elseIf(Want_To_Install($ID)) {
-    Install_msg("$ID")
+    Install_Msg("$ID")
     winget install -e --id "$ID" --source winget
 }
 br(2)
@@ -67,7 +67,7 @@ if (Test-Path  "${env:PROGRAMFILES}/Microsoft OneDrive/OneDrive.exe") {
     Update($ID)
 }
 elseIf(Want_To_Install($ID)) {
-    Install_msg("$ID")
+    Install_Msg("$ID")
     winget install -e --id "$ID" --source winget
 }
 br(2)
@@ -78,7 +78,7 @@ if (Test-Path  "${env:PROGRAMFILES}/Google/Drive File Stream/launch.bat") {
     Update($ID)
 }
 elseIf(Want_To_Install($ID)) {
-    Install_msg("$ID")
+    Install_Msg("$ID")
     winget install -e --id "$ID" --source winget
 }
 br(2)
@@ -89,7 +89,7 @@ if(Test-Path "${env:PROGRAMFILES}/7-Zip/7z.exe") {
     Update($ID)
 }
 elseIf(Want_To_Install($ID)) {
-    Install_msg("$ID")
+    Install_Msg("$ID")
     winget install -e --id "$ID" --source winget
 }
 br(2)
@@ -100,7 +100,7 @@ if(Test-Path "${env:PROGRAMFILES(x86)}/Mp3tag/Mp3tag.exe"){
     Update($ID)
 }
 elseIf(Want_To_Install($ID)) {
-    Install_msg("$ID")
+    Install_Msg("$ID")
     winget install -e --id "$ID" --source winget
 }
 br(2)
@@ -111,7 +111,7 @@ if (Test-Path "${env:PROGRAMFILES}/PowerToys/PowerToys.exe") {
     Update($ID)
 }
 elseIf(Want_To_Install($ID)) {
-    Install_msg("$ID")
+    Install_Msg("$ID")
     winget install -e --id "$ID" --source winget
 }
 br(2)
@@ -122,7 +122,7 @@ if (Test-Path  "${env:LOCALAPPDATA}/slack/slack.exe") {
     Update($ID)
 }
 elseIf(Want_To_Install($ID)) {
-    Install_msg("$ID")
+    Install_Msg("$ID")
     winget install -e --id "$ID" --source winget
 }
 br(2)
@@ -133,7 +133,7 @@ if (Test-Path  "${env:LOCALAPPDATA}/Programs/Notion/Notion.exe") {
     Update($ID)
 }
 elseIf(Want_To_Install($ID)) {
-    Install_msg("$ID")
+    Install_Msg("$ID")
     winget install -e --id "$ID" --source winget
 }
 br(2)
@@ -145,7 +145,7 @@ if (Test-Path  "${env:PROGRAMFILES}/") {
     Update($ID)
 }
 elseIf(Want_To_Install($ID)) {
-    Install_msg("$ID")
+    Install_Msg("$ID")
     winget install -e --id "$ID" --source winget
 }
 br(2)
@@ -157,52 +157,52 @@ if (Test-Path  "${env:PROGRAMFILES(X86)}/Google/Chrome/Application/chrome.exe") 
     Update($ID)
 }
 elseIf(Want_To_Install($ID)) {
-    Install_msg("$ID")
+    Install_Msg("$ID")
     winget install -e --id "$ID" --source winget
 }
 br(2)
 
 
-$ID =  Title("REALiX.HWiNFO")
+$ID = Title("REALiX.HWiNFO")
 if(Test-Path "${env:PROGRAMFILES}/HWiNFO64/HWiNFO64.EXE") {
     Update($ID)
 }
 elseIf(Want_To_Install($ID)) {
-    Install_msg("$ID")
+    Install_Msg("$ID")
     winget install -e --id "$ID" --source winget
 }
 br(2)
 
 #$ID =Title("PeterPawlowski.foobar2000")
 
-$ID =  Title("Amazon.Kindle")
+$ID = Title("Amazon.Kindle")
 if (Test-Path "${env:LOCALAPPDATA}/Amazon/Kindle/application/Kindle.exe") {
     Update($ID)
 }
 elseIf(Want_To_Install($ID)) {
-    Install_msg("$ID")
+    Install_Msg("$ID")
     winget install -e --id "$ID" --source winget
 }
 br(2)
 
 
-$ID =  Title("Microsoft.WindowsTerminal.Preview")
+$ID = Title("Microsoft.WindowsTerminal.Preview")
 if (Test-Path "${env:LOCALAPPDATA}/Microsoft/WindowsApps/Microsoft.WindowsTerminalPreview_8wekyb3d8bbwe/wt.exe") {
     Update($ID)
 }
 elseIf(Want_To_Install($ID)) {
-    Install_msg("$ID")
+    Install_Msg("$ID")
     winget install -e --id "$ID" --source winget
 }
 br(2)
 
 
-$ID =  Title("Microsoft.VisualStudio.2019.BuildTools")
+$ID = Title("Microsoft.VisualStudio.2019.BuildTools")
 if (Test-Path "${env:PROGRAMFILES(X86)}/Microsoft Visual Studio/2019/BuildTools") {
     Update($ID)
 }
 elseIf(Want_To_Install($ID)) {
-    Install_msg("$ID")
+    Install_Msg("$ID")
     winget install -e --id "$ID" --source winget
 }
 Write_Title("Visual Studio Installerから'C++ Build Tools'をダウンロードしてください.")
@@ -210,28 +210,41 @@ Write_Title("Download the 'C++ Build Tools' from Visual Studio Installer.")
 br(2)
 
 
-$ID =  Title("Microsoft.VisualStudioCode")
+$ID = Title("Microsoft.VisualStudioCode")
 if (Test-Path "${env:LOCALAPPDATA}/Programs/Microsoft VS Code/Code.exe") {
     code --version
     Update($ID)
 }
 elseIf(Want_To_Install($ID)) {
-    Install_msg("$ID")
+    Install_Msg("$ID")
     winget install -e --id "$ID" --source winget  --override "/silent /mergetasks="runcode"addcontextmenufiles,addcontextmenufolders,associatewithfiles,addtopath"""
 }
 br(2)
 
 
-$ID =  Title("Microsoft.VisualStudioCode.Insiders")
+$ID = Title("Microsoft.VisualStudioCode.Insiders")
 if (Test-Path  "${env:LOCALAPPDATA}/Programs/Microsoft VS Code Insiders/Code - Insiders.exe") {
   code-insiders --version
     Update($ID)
 }
 elseIf(Want_To_Install($ID)) {
-    Install_msg("$ID")
+    Install_Msg("$ID")
     winget install -e --id "$ID" --source winget --override "/silent /mergetasks="runcode"addcontextmenufiles,addcontextmenufolders,associatewithfiles,addtopath"""
 }
 br(2)
+
+
+<#
+$ID = Title("PeterPawlowski.foobar2000")
+if (Test-Path  "${env:LOCALAPPDATA}/") {
+    Update($ID)
+}
+elseIf(Want_To_Install($ID)) {
+    Install_Msg("$ID")
+    winget install -e --id "$ID" --source winget
+}
+br(2)
+#>
 
 
 $ID = Title("Discord.Discord")
@@ -239,88 +252,153 @@ if (Test-Path  "${env:LOCALAPPDATA}/Discord/app.ico") {
     Update($ID)
 }
 elseIf(Want_To_Install($ID)) {
-    Install_msg("$ID")
+    Install_Msg("$ID")
     winget install -e --id "$ID" --source winget
 }
 br(2)
 
 
-$ID =  Title("TeamSpeakSystems.TeamSpeakClient")
+$ID = Title("TeamSpeakSystems.TeamSpeakClient")
 if (Test-Path  "${env:PROGRAMFILES}/TeamSpeak 3 Client/ts3client_win64.exe") {
     Update($ID)
 }
 elseIf(Want_To_Install($ID)) {
-    Install_msg("$ID")
+    Install_Msg("$ID")
     winget install -e --id "$ID" --source winget
 }
 br(2)
 
 
-if ($bit -eq "x64") { $ID =  Title("Adobe.Acrobat.Reader.64-bit") }
-else{ $ID =  Title("Adobe.Acrobat.Reader.32-bit") }
+$ID = Title("Valve.Steam")
+if (Test-Path  "${env:PROGRAMFILES(X86)}/Steam/Steam.exe") {
+    Update($ID)
+}
+elseIf(Want_To_Install($ID)) {
+    Install_Msg("$ID")
+    winget install -e --id "$ID" --source winget
+}
+br(2)
+
+
+$ID = Title("ElectronicArts.EADesktop")
+if (Test-Path  "${env:PROGRAMFILES}/Electronic Arts/EA Desktop/EA Desktop/EALauncher.exe") {
+    Update($ID)
+}
+elseIf(Want_To_Install($ID)) {
+    Install_Msg("$ID")
+    winget install -e --id "$ID" --source winget
+}
+br(2)
+
+
+$ID = Title("SteelSeries.GG")
+if (Test-Path  "${env:PROGRAMFILES}/SteelSeries/GG/SteelSeriesGG.exe") {
+    Update($ID)
+}
+elseIf(Want_To_Install($ID)) {
+    Install_Msg("$ID")
+    winget install -e --id "$ID" --source winget
+}
+br(2)
+
+
+$ID = Title("Corsair.iCUE.4")
+if (test-path  "${env:PROGRAMFILES}/Corsair/CORSAIR iCUE 4 Software/iCUE.exe") {
+    update($ID)
+}
+elseif(Want_to_install($ID)) {
+    Install_Msg("$ID")
+    winget install -e --id "$ID" --source winget
+}
+br(2)
+
+
+$ID = Title("Apple.iTunes")
+if (test-path  "${env:PROGRAMFILES}/iTunes/iTunes.exe") {
+    update($ID)
+}
+elseif(Want_to_install($ID)) {
+    Install_Msg("$ID")
+    winget install -e --id "$ID" --source winget
+}
+br(2)
+
+
+if ($bit -eq "x64") { $ID = Title("Adobe.Acrobat.Reader.64-bit") }
+else{ $ID = Title("Adobe.Acrobat.Reader.32-bit") }
 if ((Test-Path  "${env:PROGRAMFILES}/Adobe/Acrobat DC/Acrobat/Acrobat.exe") -or (Test-Path  "${env:PROGRAMFILES(X86)}/Adobe/Acrobat Reader DC/Reader/AcroRd32.exe")) {
     Update($ID)
 }
 elseIf(Want_To_Install($ID)) {
-    Install_msg("$ID")
+    Install_Msg("$ID")
     winget install -e --id "$ID" --source winget
 }
 br(2)
 
 
-$ID =  Title("OpenMedia.4KVideoDownloader")
+$ID = Title("OpenMedia.4KVideoDownloader")
 if (Test-Path "${env:PROGRAMFILES}/4KDownload/4kvideodownloader/4kvideodownloader.exe") {
     Update($ID)
 }
 elseIf(Want_To_Install($ID)) {
-    Install_msg("$ID")
+    Install_Msg("$ID")
     winget install -e --id "$ID" --source winget
 }
 br(2)
 
 
-$ID =  Title("DeepL.DeepL")
+$ID = Title("DeepL.DeepL")
 if (Test-Path "${env:LOCALAPPDATA}/DeepL/DeepL.exe") {
     Update($ID)
 }
 elseIf(Want_To_Install($ID)) {
-    Install_msg("$ID")
+    Install_Msg("$ID")
     winget install -e --id "$ID" --source winget
 }
 br(2)
 
 
 <#
-$ID =  Title("kite.kite")
-If (Test-Path "${env:PROGRAMFILES}/PowerToys/PowerToys.exe") {
+$ID = Title("kite.kite")
+If (Test-Path "${env:PROGRAMFILES}/") {
   Update($ID)
 }
 elseIf(Want_To_Install($ID)) {else {
-  Install_msg("$ID")
+  Install_Msg("$ID")
   winget install -e --id "$ID" --source winget
 }
 br(2)
 #>
 
 
-$ID = Title("WiresharkFoundation.Wireshark")
-"C:\Program Files\Wireshark\Wireshark.exe"
-if (Test-Path  "${env:PROGRAMFILES}/Wireshark/Wireshark.exe") {
+$ID = Title("TranslucentTB.TranslucentTB")
+if (Test-Path  "${env:PROGRAMFILES(X86)}/TranslucentTB/TranslucentTB.exe") {
     Update($ID)
 }
 elseIf(Want_To_Install($ID)) {
-    Install_msg("$ID")
+    Install_Msg("$ID")
     winget install -ei --id "$ID" --source winget
 }
 br(2)
 
 
-$ID =  Title("Microsoft.PowerShell")
+$ID = Title("WiresharkFoundation.Wireshark")
+if (Test-Path  "${env:PROGRAMFILES}/Wireshark/Wireshark.exe") {
+    Update($ID)
+}
+elseIf(Want_To_Install($ID)) {
+    Install_Msg("$ID")
+    winget install -ei --id "$ID" --source winget
+}
+br(2)
+
+
+$ID = Title("Microsoft.PowerShell")
 if (Test-Path  "${env:PROGRAMFILES}/PowerShell/7/pwsh.exe") {
     Update($ID)
 }
 elseIf(Want_To_Install($ID)) {
-    Install_msg("$ID")
+    Install_Msg("$ID")
     winget install -ei --id "$ID" --source winget
 }
 br(2)
