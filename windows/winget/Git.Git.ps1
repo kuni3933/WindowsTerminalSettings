7a99818192e5fad43e42d508b95df82b8ae0d7e1
kuni3933 <57650186+kuni3933@users.noreply.github.com>
2022-02-07 11:53:43 +0900

    [Update] JSON内の環境変数の書き方を%%で囲むDOS式に変更
    Signed-off-by: kuni3933 <57650186+kuni3933@users.noreply.github.com>

--- windows/winget/Git.Git.ps1
+++ windows/winget/Git.Git.ps1
@@ -1,6 +1,6 @@
 ${MyPath} = Split-Path -Parent $MyInvocation.MyCommand.Path
 
-& "${env:GIT_INSTALL_ROOT}/git-bash.exe ${MyPath}/Git.Git.sh"
+&"${env:GIT_INSTALL_ROOT}/git-bash.exe" "${MyPath}/Git.Git.sh"
 
 [System.Diagnostics.Process] ${process} = Start-Process `
 		-FilePath "git.exe" `
