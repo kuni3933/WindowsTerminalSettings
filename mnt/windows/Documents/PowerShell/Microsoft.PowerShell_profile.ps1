3dae4dfc5b256f26949c1f78983bce655d481b2a
kuni3933 <57650186+kuni3933@users.noreply.github.com>
2022-07-20 14:16:23 +0900

    [Update] mnt/windows/Documents/PowerShell/Microsoft.PowerShell_profile.ps1
    Signed-off-by: kuni3933 <57650186+kuni3933@users.noreply.github.com>

--- mnt/windows/Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ mnt/windows/Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -237,9 +237,9 @@ function gbm()  { git branch -l | rg -v '^\* ' | ForEach-Object { $_ -replace " 
 # git log
 function gls()   { git log -3}
 function gll()   { git log -10 --oneline --all --graph --decorate }
-function glll()  { git log --graph --all --date=format:'%Y-%m-%d %H:%M' --pretty=format:'%C(auto)%d%Creset\ %C(yellow)%h%Creset %C(magenta)%ae%Creset %C(cyan)%ad%Creset%n%C(white bold)%w(80)%s%Creset%n%b' }
-function glls()  { git log --graph --all --date=format:'%Y-%m-%d %H:%M' --pretty=format:'%C(auto)%d%Creset\ %C(yellow)%h%Creset %C(magenta)%ae%Creset %C(cyan)%ad%Creset%n%C(white bold)%w(80)%s%Creset%n%b' -10}
-function ggraph()  { git log --graph --all  --date=format:'%Y-%m-%d %H:%M' -C -M --pretty=format:\"<%h> %ad [%an] %Cgreen%d%Creset %s\" --date-order }
+function glll()  { git log --graph --all --date=format:'%Y-%m-%d %H:%M:%S' --pretty=format:'%C(auto)%d%Creset\ %C(yellow)%h%Creset %C(magenta)%ae%Creset %C(cyan)%ad%Creset%n%C(white bold)%w(80)%s%Creset%n%b' }
+function glls()  { git log --graph --all --date=format:'%Y-%m-%d %H:%M:%S' --pretty=format:'%C(auto)%d%Creset\ %C(yellow)%h%Creset %C(magenta)%ae%Creset %C(cyan)%ad%Creset%n%C(white bold)%w(80)%s%Creset%n%b' -10}
+function ggraph()  { git log --graph --all  --date=format:'%Y-%m-%d %H:%M:%S' -C -M --pretty=format:\"<%h> %ad [%an] %Cgreen%d%Creset %s\" --date-order }
 
 # git status
 function gs()  { git status --short }
